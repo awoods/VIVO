4cc5637f88320bae5b7bb6161264fd266f57855b
Andrew Woods <awoods@duraspace.org>
2019-03-21 19:29:14 -0400

    Add script dependencies

--- scripts/run-selenium.sh
+++ scripts/run-selenium.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 set -ex
-echo "Running Selenium tests"
-echo "...Running Selenium tests"
+echo "Running Selenium tests - start"
+
+git clone https://github.com/vivo-community/vivo-acceptance-tests.git
+
+ruby -v
+apt-get update
+apt-get install ruby-full -y
+apt-get install chromium-chromedriver
+
+gem install rspec
+gem install selenium-webdriver
+
+
+echo "Running Selenium tests - end"
